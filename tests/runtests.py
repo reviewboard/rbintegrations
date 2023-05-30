dd70cfee80f9da30e9f45b6aea90d90bdf29f8c2
David Trowbridge <trowbrds@gmail.com>
2023-05-30 14:52:23 -0600

    Update for new pytest-based test runner.
    This adds the conftest.py and setup.cfg needed to run the rbintegrations
    test suite with pytest (either by running `rbext test -m rbintegrations`
    or just `pytest`).
    
    Testing Done:
    Ran unit tests.
    
    Reviewed at https://reviews.reviewboard.org/r/13631/

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
+import pytest
 import reviewboard
-from reviewboard.cmdline.rbext import RBExt
-
-
-def main(argv):
-    print('Python %s.%s.%s' % (sys.version_info[:3]))
-    print('Review Board %s' % reviewboard.get_version_string())
-
-    # When we drop Review Board 3.x support, we can remove -s and -m and
-    # switch to:
-    #
-    #     --app beanbag_licensing.tests
-    #     -e rbpowerpack.extension.PowerPackExtension
-    sys.exit(RBExt().run([
-        'test',
-        '-m', 'rbintegrations',
-        '--',
-    ] + argv))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    print('Python %s.%s.%s' % (sys.version_info[:3]))
+    print('Review Board %s' % reviewboard.get_version_string())
+
+    sys.exit(pytest.main(sys.argv[1:]))
