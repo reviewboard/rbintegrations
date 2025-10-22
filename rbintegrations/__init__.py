ad3b4553c803c9327d6e8dd4b398af34c1ebfcfe
David Trowbridge <david@beanbaginc.com>
2025-10-22 15:41:13 -0600

    Move rbintegrations over to pyproject.toml.
    This change ports rbintegrations over to the new extension packaging
    build backend.
    
    This also bumps our version to 5.0dev.
    
    Testing Done:
    Did a package build.
    
    Reviewed at https://reviews.reviewboard.org/r/14849/

--- rbintegrations/__init__.py
+++ rbintegrations/__init__.py
@@ -1,82 +1,22 @@
-"""RBIntegrations version and package information.
+"""Main import for RBIntegrations."""
 
-These variables and functions can be used to identify the version of
-the module. They're largely used for packaging purposes.
-"""
+from __future__ import annotations
 
-
-#: The version of RBIntegrations.
-#:
-#: This is in the format of:
-#:
-#: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
-#:
-VERSION = (4, 0, 3, 0, 'alpha', 0, False)
+from rbintegrations._version import (
+    VERSION,
+    __version__,
+    __version_info__,
+    get_package_version,
+    get_version_string,
+    is_release,
+)
 
 
-def get_version_string():
-    """Return the version as a human-readable string.
-
-    Returns:
-        unicode:
-        The human-readable version.
-    """
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-
-    if VERSION[2] or VERSION[3]:
-        version += ".%s" % VERSION[2]
-
-    if VERSION[3]:
-        version += ".%s" % VERSION[3]
-
-    if VERSION[4] != 'final':
-        if VERSION[4] == 'rc':
-            version += ' RC%s' % VERSION[5]
-        else:
-            version += ' %s %s' % (VERSION[4], VERSION[5])
-
-    if not is_release():
-        version += " (dev)"
-
-    return version
-
-
-def get_package_version():
-    """Return the version as a Python package version string.
-
-    Returns:
-        unicode:
-        The package version.
-    """
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-
-    if VERSION[2] or VERSION[3]:
-        version += ".%s" % VERSION[2]
-
-    if VERSION[3]:
-        version += ".%s" % VERSION[3]
-
-    if VERSION[4] != 'final':
-        version += '%s%s' % (VERSION[4], VERSION[5])
-
-    return version
-
-
-def is_release():
-    """Return whether this is a released version.
-
-    Returns:
-        bool:
-        ``True`` if this is a released version, or ``False`` if it's still
-        in development.
-    """
-    return VERSION[6]
-
-
-#: An alias for the the version information from :py:data:`VERSION`.
-#:
-#: This does not include the last entry in the tuple (the released state).
-__version_info__ = VERSION[:-1]
-
-#: An alias for the version used for the Python package.
-__version__ = get_package_version()
+__all__ = [
+    'VERSION',
+    '__version__',
+    '__version_info__',
+    'get_package_version',
+    'get_version_string',
+    'is_release',
+]
---
+++ rbintegrations/_version.py
@@ -0,0 +1,90 @@
+"""RBIntegrations version and package information.
+
+These variables and functions can be used to identify the version of
+the module. They're largely used for packaging purposes.
+"""
+
+from __future__ import annotations
+
+
+#: The version of RBIntegrations.
+#:
+#: This is in the format of:
+#:
+#: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
+#:
+VERSION: tuple[int, int, int, int, str, int, bool] = \
+    (5, 0, 0, 0, 'alpha', 0, False)
+
+
+def get_version_string() -> str:
+    """Return the version as a human-readable string.
+
+    Returns:
+        str:
+        The human-readable version.
+    """
+    major, minor, micro, patch, tag, release_num, released = VERSION
+
+    version = f'{major}.{minor}'
+
+    if micro or patch:
+        version += f'.{micro}'
+
+    if patch:
+        version += f'.{patch}'
+
+    if tag != 'final':
+        if tag == 'rc':
+            version += f' RC{release_num}'
+        else:
+            version += f' {tag} {release_num}'
+
+    if not released:
+        version += ' (dev)'
+
+    return version
+
+
+def get_package_version() -> str:
+    """Return the version as a Python package version string.
+
+    Returns:
+        str:
+        The package version.
+    """
+    major, minor, micro, patch, tag, release_num = VERSION[:6]
+
+    version = f'{major}.{minor}'
+
+    if micro or patch:
+        version += f'.{micro}'
+
+    if patch:
+        version += f'.{patch}'
+
+    if tag != 'final':
+        version += f'{tag}{release_num}'
+
+    return version
+
+
+def is_release() -> bool:
+    """Return whether this is a released version.
+
+    Returns:
+        bool:
+        ``True`` if this is a released version, or ``False`` if it's still
+        in development.
+    """
+    return VERSION[6]
+
+
+#: An alias for the the version information from :py:data:`VERSION`.
+#:
+#: This does not include the last entry in the tuple (the released state).
+__version_info__ = VERSION[:-1]
+
+
+#: An alias for the version used for the Python package.
+__version__ = get_package_version()
